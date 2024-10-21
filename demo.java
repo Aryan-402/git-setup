fcf5465c63c52e87f6f773358acb770852acbda9
Aryan <aryanbhaskar003@gmail.com>
2024-10-21 15:15:51 +0530

    adding a new sum variable in demo.java

--- demo.java
+++ demo.java
@@ -2,7 +2,8 @@ public class demo {
     public static void main(String[] args) {
         int a=6;
         int b=7;
-        int sum = a+b;
+        int c=5;
+        int sum = a+b+c†;
         System.out.println("sum is : " + sum);
     }
 }
